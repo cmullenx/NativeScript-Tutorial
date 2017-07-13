252ce63e74bf6c79a86b7c68738fc0c7ca31eabb
Christina mullen <cmullenx@gmail.com>
2017-07-13 16:52:02 -0500

    first commit

---
+++ platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,10 @@
+dependencies: \
+  /Users/christinamullen/Documents/GitRepos/Groceries/platforms/ios/Groceries/Groceries-Prefix.pch \
+  /Users/christinamullen/Documents/GitRepos/Groceries/platforms/ios/internal/main.m \
+  /Users/christinamullen/Documents/GitRepos/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/christinamullen/Documents/GitRepos/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/christinamullen/Documents/GitRepos/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/christinamullen/Documents/GitRepos/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/christinamullen/Documents/GitRepos/Groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/christinamullen/Documents/GitRepos/Groceries/platforms/ios/internal/TNSDebugging.h \
+  /Users/christinamullen/Documents/GitRepos/Groceries/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
